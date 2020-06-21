08d2703ddd4c71f54fde2a6b1fd8adfd46a889cf
nikseljkee <nikseljkee1@gmail.com>
2020-06-21 17:31:48 +0300

    Added Repository pattern

---
+++ SimpleREST_API/Services/IProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using SimpleREST_API.Entities;
+using System.Collections.Generic;
+
+namespace SimpleREST_API.Services
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProduct(int productId);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(Product product);
+
+        IEnumerable<Company> GetCompanies();
+        Company GetCompany(int companyId);
+        void AddCompany(Company company);
+        void UpdateCompany(Company company);
+        void DeleteCompany(Company company);
+
+        bool ProductExists(int productId);
+        bool CompanyExists(int companyId);
+
+        bool Save();
+    }
+}
---
+++ SimpleREST_API/Services/ProductRepository.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleREST_API.Data;
+using SimpleREST_API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleREST_API.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _db;
+        public ProductRepository(ProductContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public void AddCompany(Company company)
+        {
+            if (company == null)
+                throw new ArgumentNullException(nameof(company));
+
+            _db.Companies.Add(company);
+        }
+
+        public void AddProduct(Product product)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+
+            _db.Products.Add(product);
+        }
+
+        public bool CompanyExists(int companyId)
+        {
+            return _db.Companies.Any(c => c.Id == companyId);
+        }
+
+        public void DeleteCompany(Company company)
+        {
+            if (company == null)
+                throw new ArgumentNullException(nameof(company));
+
+            _db.Companies.Remove(company);
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+
+            _db.Products.Remove(product);
+        }
+
+        public IEnumerable<Company> GetCompanies()
+        {
+            return _db.Companies.ToList();
+        }
+
+        public Company GetCompany(int companyId)
+        {
+            return _db.Companies.FirstOrDefault(c => c.Id == companyId);
+        }
+
+        public Product GetProduct(int productId)
+        {
+            return _db.Products.FirstOrDefault(p => p.Id == productId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _db.Products.ToList();
+        }
+
+        public bool ProductExists(int productId)
+        {
+            return _db.Products.Any(p => p.Id == productId);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0;
+        }
+
+        public void UpdateCompany(Company company)
+        {            
+        }
+
+        public void UpdateProduct(Product product)
+        {
+        }
+    }
+}
